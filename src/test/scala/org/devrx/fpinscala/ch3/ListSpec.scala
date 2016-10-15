95e09fed89c69af85c51d28c5951bcc5d1ae0d1f
Francis Devereux <francis@devrx.org>
2016-10-15 14:08:25 +0100

    Ex3.2: List.tail()

---
+++ src/test/scala/org/devrx/fpinscala/ch3/ListSpec.scala
@@ -0,0 +1,16 @@
+package org.devrx.fpinscala.ch3
+
+import org.scalatest.{Matchers, WordSpec}
+
+class ListSpec extends WordSpec with Matchers {
+  "tail" should {
+    "return the tail of the list for a non-empty-list" in {
+      List.tail(List(1,2,3)) shouldBe List(2,3)
+    }
+    "throw an exception for an empty list" in {
+      intercept[IllegalArgumentException] {
+        List.tail(List())
+      }
+    }
+  }
+}
