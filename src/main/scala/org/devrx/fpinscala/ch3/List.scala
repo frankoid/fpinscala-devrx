d0425d36cd84b5e88c67f7a837b3bc3429357cae
Francis Devereux <francis@devrx.org>
2016-10-15 14:18:43 +0100

    Ex3.3: List.setHead()

--- src/main/scala/org/devrx/fpinscala/ch3/List.scala
+++ src/main/scala/org/devrx/fpinscala/ch3/List.scala
@@ -55,7 +55,10 @@ object List { // `List` companion object. Contains functions for creating and wo
     case Cons(x, xs) => xs
   }
 
-  def setHead[A](l: List[A], h: A): List[A] = sys.error("todo")
+  def setHead[A](l: List[A], h: A): List[A] = l match {
+    case Nil => throw new UnsupportedOperationException("Can't set the head of an empty list")
+    case Cons(x, xs) => Cons(h, xs)
+  }
 
   def drop[A](l: List[A], n: Int): List[A] = sys.error("todo")
 
--- src/test/scala/org/devrx/fpinscala/ch3/ListSpec.scala
+++ src/test/scala/org/devrx/fpinscala/ch3/ListSpec.scala
@@ -4,12 +4,23 @@ import org.scalatest.{Matchers, WordSpec}
 
 class ListSpec extends WordSpec with Matchers {
   "tail" should {
-    "return the tail of the list for a non-empty-list" in {
+    "return the tail of the list for a non-empty list" in {
       List.tail(List(1,2,3)) shouldBe List(2,3)
     }
     "throw an exception for an empty list" in {
       intercept[UnsupportedOperationException] {
         List.tail(List())
+      }
+    }
+  }
+
+  "setHead" should {
+    "set the first element of the list for a non-empty list" in {
+      List.setHead(List(1,2,3), 10) shouldBe List(10,2,3)
+    }
+    "throw an exception for an empty list" in {
+      intercept[UnsupportedOperationException] {
+        List.setHead(List(), 10)
       }
     }
   }
